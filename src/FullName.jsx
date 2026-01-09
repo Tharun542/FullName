cec51153e917eafbd3cf688dced175c2765af330
tharun <godasutharun143@gmail.com>
2026-01-09 12:57:18 +0530

    third commit updated display name

--- src/FullName.jsx
+++ src/FullName.jsx
@@ -1,37 +1,50 @@
-import React, {  useState } from "react";
+import React, { useState } from "react";
 import "./fullname.css";
 
-export default function FullName(){
-    
-    const [fname, setFname] = useState("");
-    const [lname, setLname] = useState("");
-    const [selectedName, setSelectedName] = useState("");
-    
-  const handleName=(e)=>{
+export default function FullName() {
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [selectedName, setSelectedName] = useState("");
+
+  const handleName = (e) => {
     e.preventDefault();
+
     const first = fname.trim();
     const last = lname.trim();
-    if(!fname || !lname){
-     setSelectedName("")
-     return;
-    }
-    setSelectedName(first+ " "+ last);
-  }
-   
 
-    return(
-       <form className="form" onSubmit={handleName} noValidate>
-        <div>
-            <label htmlFor="">FullName:</label>
-            <input type="text" value={fname} onChange={(e)=> setFname(e.target.value)}/>
-        </div>
-        <div>
-            <label htmlFor="">LastName:</label>
-            <input type="text" value={lname} onChange={(e)=> setLname(e.target.value)}/>
-        </div>
-        
-        <button type="submit">submit</button>
-        {selectedName && (<h1>FULLNAME : {selectedName}</h1>)}
-       </form>
-    )
+    if (!first || !last) {
+      setSelectedName("");
+      return;
+    }
+
+    setSelectedName(first + " " + last);
+  };
+
+  return (
+    <form className="form" onSubmit={handleName} noValidate>
+      <div>
+        <label>First Name</label>
+        <input
+          type="text"
+          placeholder="First Name"
+          value={fname}
+          onChange={(e) => setFname(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <label>Last Name</label>
+        <input
+          type="text"
+          placeholder="Last Name"
+          value={lname}
+          onChange={(e) => setLname(e.target.value)}
+        />
+      </div>
+
+      <button type="submit">Submit</button>
+
+      {selectedName && <h1>FULLNAME : {selectedName}</h1>}
+    </form>
+  );
 }
